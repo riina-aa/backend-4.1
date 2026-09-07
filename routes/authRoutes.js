c61177ca7d77815a779b6d6724a48f7e9ed09e3e
Riina <riina.e.aaltonen@gmail.com>
2026-09-07 15:34:37 +0200

    Kodat klart register/login routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,11 +3,57 @@ const router = express.Router();
 const Users = require("../models/userSchema");
 
 router.post("/register", async (req, res) => {
-    res.json({message: "Registrering startad..."})
+    
+    try {
+        const { username, password } = req.body; 
+
+        //Validerar input
+        if(!username || !password ) {
+            return res.status(400).json({ message: "Användarnamn eller lösenord får inte vara tomt."}); 
+        }
+
+        //Korrekt input - spara användare 
+        const user = new User({ username, password}); 
+        await user.save(); 
+
+        res.status(200).json({ message: "Användaruppgifterna är sparade."}); 
+
+    } catch (error) {
+        
+        res.status(500).json({ message: "Serverfel. Vänligen pröva igen om en stund."})
+    }
 }); 
 
 router.post("/login", async (req, res) => {
-    res.json({message: "Inloggning startad..."})
+    
+    try {
+        const { username, password } = req.body; 
+
+        //Validerar input
+        if(!username || !password ) {
+            return res.status(400).json({ message: "Användarnamn eller lösenord får inte vara tomt."}); 
+        }
+
+        //Kontrollerar inloggningsuppgifter 
+        const user = await Users.findOne({ username}); 
+        
+        if(!user) {
+            return res.status(401).json({ message: "Användarnamn eller lösenord är fel."}); 
+        }
+
+        const isPasswordMatch = await Users.comparePassword(password); 
+
+        if(!isPasswordMatch) {
+            return res.status(401).json({ message: "Användarnamn eller lösenord är fel."}); 
+        } else {
+            res.status(200).json({ message: "Inloggningen lyckades"}); 
+        }
+
+
+
+    } catch (error) {
+        
+    }
 })
 
 module.exports = router; 
